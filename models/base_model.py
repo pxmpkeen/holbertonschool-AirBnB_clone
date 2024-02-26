f4fda3e7652b3dad4d1f049d021205727a0da86b
gulrah <gulnarrahimli@mail.ru>
2024-02-26 18:35:31 +0000

    BaseModel Updated

--- models/base_model.py
+++ models/base_model.py
@@ -15,7 +15,15 @@ class BaseModel:
             self.id = str(uuid.uuid4())
         if created_at is not None:
             self.created_at = datetime.now()
-        self.updated_at = self.created_at
+        self.updated_at = datetime.now()
     def __str__(self):
+        return "[{}} ({}) {}".format(
+            type(self).__name__, self.id, self.__dict__)
     def to_dict(self):
+        obj_dict = self.__dict__.copy()
+        obj_dict['__class__'] = type(self).__name__
+        obj_dict['created_at'] = self.created_at.isoformat()
+        obj_dict['updated_at'] = self.updated_at.isoformat()
+        return obj_dict
     def save(self):
+        self.updated_at = datetime.now()
