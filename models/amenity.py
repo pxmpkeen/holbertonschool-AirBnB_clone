6204be68f47051b83ee0fd95f5af9255acc2dbae
gulrah <gulnarrahimli@mail.ru>
2024-02-26 20:39:03 +0000

    Files were edited

---
+++ models/amenity.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Amenity"
+from models.base_model import BaseModel
+
+
+class Amenity(BaseModel):
+    name = ""
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
