4c66a6cdcbdd1783f8a6128777c9853a12b0b24f
gulrah <gulnarrahimli@mail.ru>
2024-02-26 20:22:35 +0000

    Key was added

--- models/base_model.py
+++ models/base_model.py
@@ -5,7 +5,7 @@ import models
 
 
 class BaseModel:
-    def __init__(self, id=None, created_at=None, updated_at=None):
+    def __init__(self, id=None, created_at=None, updated_at=None, **kwargs):
         if id is not None:
             self.id = id
         else:
