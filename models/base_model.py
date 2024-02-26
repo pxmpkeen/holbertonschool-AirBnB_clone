8f9147485e2fc242ea49208d5675a7e3cf16de9d
gulrah <gulnarrahimli@mail.ru>
2024-02-26 20:15:49 +0000

    Key was added

--- models/base_model.py
+++ models/base_model.py
@@ -23,6 +23,7 @@ class BaseModel:
             type(self).__name__, self.id, self.__dict__)
     def to_dict(self):
         obj_dict = self.__dict__.copy()
+        obj_dict['__class__'] = type(self).__name__
         obj_dict['created_at'] = self.created_at.isoformat()
         obj_dict['updated_at'] = self.updated_at.isoformat()
         return obj_dict
