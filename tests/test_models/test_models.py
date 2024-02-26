50a52c195eaafd3d1a70c98d570be41dd814fdd8
gulrah <gulnarrahimli@mail.ru>
2024-02-26 20:45:12 +0000

    Test Files

---
+++ tests/test_models/test_models.py
@@ -0,0 +1,41 @@
+import unittest
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.place import Place
+from models.review import Review
+
+
+class TestModels(unittest.TestCase):
+    def test_state(self):
+        state = State()
+        self.assertEqual(state.name, "")
+    def test_city(self):
+        city = City()
+        self.assertEqual(city.state_id, "")
+        self.assertEqual(city.name, "")
+    def test_amenity(self):
+        amenity = Amenity()
+        self.assertEqual(amenity.name, "")
+    def test_place(self):
+        place = Place()
+        self.assertEqual(place.city_id, "")
+        self.assertEqual(place.user_id, "")
+        self.assertEqual(place.name, "")
+        self.assertEqual(place.description, "")
+        self.assertEqual(place.number_rooms, 0)
+        self.assertEqual(place.number_bathrooms, 0)
+        self.assertEqual(place.max_guest, 0)
+        self.assertEqual(place.price_by_night, 0)
+        self.assertEqual(place.latitude, 0.0)
+        self.assertEqual(place.longitude, 0.0)
+        self.assertEqual(place.amenity_ids, [])
+    def test_review(self):
+        review = Review()
+        self.assertEqual(review.place_id, "")
+        self.assertEqual(review.user_id, "")
+        self.assertEqual(review.text, "")
+        
+        
+    if __name__ == '__main__':
+        unittest.main()
