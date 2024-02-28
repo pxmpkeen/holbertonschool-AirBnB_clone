3906a6c305059d6533a109b0d02d80734599b3b4
pxmpkeen <pxmpkeen@gmail.com>
2024-02-28 15:18:20 +0400

    console 0.1 implementation added

---
+++ console.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+import cmd
+
+
+class HBNBCommand(cmd.Cmd):
+    prompt = '(hbnb) '
+
+    def do_quit(self, str):
+        """Quit the program."""
+        exit()
+
+    def do_EOF(self, str):
+        """Quit the program."""
+        exit()
+
+    def emptyline(self):
+        """Catches enter"""
+        pass
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
