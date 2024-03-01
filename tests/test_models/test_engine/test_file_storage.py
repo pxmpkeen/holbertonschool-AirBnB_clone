85da7099495162ba37c1a1d968d249b713bc2832
gulrah <gulnarrahimli@mail.ru>
2024-03-01 11:33:46 +0000

    a little edit

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -3,7 +3,7 @@ import unittest
 import os
 from models.base_model import BaseModel
 from models.user import User
-from models.file_storage import FileStorage
+from models.engine.file_storage import FileStorage
 
 
 class TestFileStorage(unittest.TestCase):
